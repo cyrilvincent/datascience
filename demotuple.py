0779f006c6adfbd4ee378d97db856242097aac7b
cyrilvincent <contact@cyrilvincent.com>
2021-01-11 13:39:03 +0100

    CEA

--- demotuple.py
+++ demotuple.py
@@ -1,12 +1,33 @@
 def min_max_avg(l):
-    return 0,99,50
+    min = l[0]
+    max = l[0]
+    sum = 0
+    for i in l:
+        if i < min:
+            min = i
+        elif i > max:
+            max = i
+        sum += i
+    return min, max, sum/len(l)
+
 
 def min_max_avg_dico(l):
-    return {"min":0,"max":99,"avg":50}
+    min = l[0]
+    max = l[0]
+    sum = 0
+    for i in l:
+        if i < min:
+            min = i
+        if i > max:
+            max = i
+        sum += i
+    return {"min": min, "max": max, "avg": sum/len(l)}
+
 
 def per_tuple(*kargs):
     for param in kargs:
         print(param)
+
 
 def per_dict(**kwargs):
     print(kwargs)
@@ -14,17 +35,19 @@ def per_dict(**kwargs):
         print(param)
         print(kwargs[param])
 
+
 def the_bordel(a,*kargs,**kwargs):
     print(a)
     print(kargs)
     print(kwargs)
+
 
 if __name__ == '__main__':
     min, max, avg = min_max_avg(range(100))
     print(min, max, avg)
     dico = min_max_avg_dico(range(100))
     print(dico["min"])
-    per_tuple(1,4,5,6,99)
+    per_tuple(1, 4, 5, 6, 99)
     per_dict(toto=1, titi=4)
-    the_bordel(1,2,3,4,5,toto=1, titi=4)
-    
+    the_bordel(1, 2, 3, 4, 5, toto=1, titi=4)
+
