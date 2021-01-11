b746bdfa07c11c6bfd50ba289763235c20326763
cyrilvincent <contact@cyrilvincent.com>
2021-01-11 14:39:38 +0100

    CEA

--- tpcovid.py
+++ tpcovid.py
@@ -5,3 +5,10 @@ with open("data/covid/covid-france.txt") as f:
     for row in reader:
         if int(row["NbCas"]) != 0:
             print(int(row["DC"]) / int(row["NbCas"]))
+
+# Créer 3 listes : ixs, nbcas, dcs
+# Caster les listes en np.array
+# nbcas max, dc max
+# Filtrer nbcas, ixs et dcs pour nbcas > 10
+# Sur ces filtres créer le np.array letality et l'afficher
+# Afficher la letalité min, max
