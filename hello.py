2bac5bb1691cb20dc9917d7613d93e696b0c5a1b
cyrilvincent <contact@cyrilvincent.com>
2021-01-11 12:57:54 +0100

    CEA

--- hello.py
+++ hello.py
@@ -7,3 +7,20 @@ def is_even():
 print("Hello World")
 print(sys.version)
 print(toto.is_even(7))
+
+l1 = [1,2,3,4]
+l2 = l1
+l2.append(5)
+print(l1, l2)
+print(l1 == l2)
+print(l1 is l2)
+
+l1 = [1,2,3,4]
+l2 = list(l1)
+l1.append(5)
+l2.append(5)
+print(l1, l2)
+print(l1 == l2)
+print(l1 is l2)
+print(type(l1))
+
