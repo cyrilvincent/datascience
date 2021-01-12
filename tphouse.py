c53793a3e7fb8cfce3fe2ab64f14c572550b164e
cyrilvincent <contact@cyrilvincent.com>
2021-01-12 11:52:27 +0100

    CEA

---
+++ tphouse.py
@@ -0,0 +1,8 @@
+# Charger data/house/house.csv et créer les vecteurs surfaces, loyers
+# Afficher le nuage de points surface / loyer
+# min, max, avg, std, media, quartile sur loyer et surface
+# loyerperm2 = loyer / surface
+# min, max, avg, std, media, quartile sur loyerperm2
+# Normal?
+# Filtrer les points en fonction de ce que vous déciderez
+# min, max, avg, std, media, quartile sur loyerperm2 filtré
