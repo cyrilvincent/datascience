fe3dde2e37d44cc857f524efcf446bf295b1ab9f
cyrilvincent <contact@cyrilvincent.com>
2021-01-12 13:52:58 +0100

    CEA

--- tphouse.py
+++ tphouse.py
@@ -23,15 +23,15 @@ loyers = np.array(loyers)
 
 print(f"Loyer min:{np.min(loyers)}, max:{np.max(loyers)}, avg:{np.mean(loyers)}, std:{np.std(loyers)}, median:{np.median(loyers)}, quartiles:{np.quantile(loyers, [0.25,0.75])}")
 print(f"Surface min:{np.min(surfaces)}, max:{np.max(surfaces)}, avg:{np.mean(surfaces)}, std:{np.std(surfaces)}, median:{np.median(surfaces)}, quartiles:{np.quantile(surfaces, [0.25,0.75])}")
-# plt.scatter(surfaces, loyers)
-# plt.show()
+plt.scatter(surfaces, loyers)
+plt.show()
 
 loyersperm2 = loyers / surfaces
 print(f"Loyerperm2 min:{np.min(loyersperm2)}, max:{np.max(loyersperm2)}, avg:{np.mean(loyersperm2)}, std:{np.std(loyersperm2)}, median:{np.median(loyersperm2)}, quartiles:{np.quantile(loyersperm2, [0.25,0.75])}")
 plt.scatter(np.arange(len(loyersperm2)), loyersperm2)
 plt.show()
-loyersperm2_filter = loyersperm2[loyers < 200]
-loyersperm2_filter = loyersperm2[loyersperm2 < 38 + 3 * 10]
+loyersperm2_filter = loyersperm2[surfaces < 200]
+loyersperm2_filter = loyersperm2_filter[loyersperm2_filter < 38 + 3 * 10]
 print(f"Loyerperm2_filter min:{np.min(loyersperm2_filter)}, max:{np.max(loyersperm2_filter)}, avg:{np.mean(loyersperm2_filter)}, std:{np.std(loyersperm2_filter)}, median:{np.median(loyersperm2_filter)}, quartiles:{np.quantile(loyersperm2_filter, [0.25,0.75])}")
 plt.scatter(np.arange(len(loyersperm2_filter)), loyersperm2_filter)
 plt.show()
