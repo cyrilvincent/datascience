639107c27f7c0c41d922317071f4b7c904e92fe5
cyrilvincent <contact@cyrilvincent.com>
2021-01-13 15:30:26 +0100

    CEA

--- tp-cancer-rf.py
+++ tp-cancer-rf.py
@@ -9,16 +9,7 @@ np.random.seed(0)
 y = data.diagnosis
 x = data.drop("diagnosis", 1)
 
-# Knn avec k = 3
-# fit, score
-# Trouver le k optimum k=1..15
-# Humain = 95%
-
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
-# for k in range(1,16):
-#     model = nn.KNeighborsClassifier(n_neighbors=k)
-#     model.fit(xtrain, ytrain)
-#     print(k, model.score(xtest, ytest))
 model = nn.KNeighborsClassifier(n_neighbors=8)
 model.fit(xtrain, ytrain)
 print(model.score(xtest, ytest))
