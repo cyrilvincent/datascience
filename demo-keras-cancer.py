1f2ddc1ed0d76f421e81b68cc44dd26616cffb05
cyrilvincent <contact@cyrilvincent.com>
2021-01-14 15:02:39 +0100

    CEA

---
+++ demo-keras-cancer.py
@@ -0,0 +1,25 @@
+import tensorflow.keras as keras
+import pandas
+import numpy as np
+import sklearn.model_selection as ms
+
+data = pandas.read_csv("data/breast-cancer/data.csv", index_col="id")
+np.random.seed(0)
+
+y = data.diagnosis
+x = data.drop("diagnosis", 1)
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
+
+model = keras.Sequential([
+    keras.layers.Dense(30, activation="relu",
+                       input_shape=(xtrain.shape[1],)),
+    keras.layers.Dense(10, activation="relu"),
+    keras.layers.Dense(1)
+  ])
+
+model.compile(loss="mse", optimizer="rmsprop",metrics=['accuracy'])
+model.summary()
+
+model.fit(xtrain, ytrain, epochs=200)
+print(model.evaluate(xtest, ytest))
