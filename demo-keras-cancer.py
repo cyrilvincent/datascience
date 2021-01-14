4788daf3a5043fc38132ec9c140e820477bab493
cyrilvincent <contact@cyrilvincent.com>
2021-01-14 16:06:13 +0100

    CEA

--- demo-keras-cancer.py
+++ demo-keras-cancer.py
@@ -2,6 +2,7 @@ import tensorflow.keras as keras
 import pandas
 import numpy as np
 import sklearn.model_selection as ms
+import sklearn.preprocessing as pp
 
 data = pandas.read_csv("data/breast-cancer/data.csv", index_col="id")
 np.random.seed(0)
@@ -9,17 +10,23 @@ np.random.seed(0)
 y = data.diagnosis
 x = data.drop("diagnosis", 1)
 
+scaler = pp.RobustScaler()
+scaler.fit(x)
+x = scaler.transform(x)
+
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
 
 model = keras.Sequential([
     keras.layers.Dense(30, activation="relu",
                        input_shape=(xtrain.shape[1],)),
     keras.layers.Dense(10, activation="relu"),
-    keras.layers.Dense(1)
+    keras.layers.Dense(1, activation="sigmoid")
   ])
 
-model.compile(loss="mse", optimizer="rmsprop",metrics=['accuracy'])
+model.compile(loss="binary_crossentropy", optimizer="rmsprop",metrics=['accuracy'])
 model.summary()
 
-model.fit(xtrain, ytrain, epochs=200)
-print(model.evaluate(xtest, ytest))
+model.fit(xtrain, ytrain, epochs=200, validation_split=0.2)
+print(model.evaluate(xtrain, ytrain))
+
+fnorm = lambda x, avg, std : (x - avg) / std
