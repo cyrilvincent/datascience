c4d3056e5b1c8917d87a4333a7ec306ce3883a2d
Cyril Vincent <contact@cyrilvincent.com>
2026-09-08 22:14:40 +0200

    Sparks

--- democomplex.py
+++
@@ -1,6 +0,0 @@
-x = 1j ** 2
-print(x)
-y = 3-4j
-print(y)
-print(x + y)
-print(x * y)
--- hello.py
+++ hello.py
@@ -1,27 +1,4 @@
 import sys
-import mylib as toto
 
-def is_even():
-    pass
-
-print("Hello World")
+print("Hello")
 print(sys.version)
-print(toto.is_even(7))
-
-l1 = [1,2,3,4]
-l2 = l1
-l2.append(5)
-print(l1, l2)
-print(l1 == l2)
-print(l1 is l2)
-
-l1 = [1,2,3,4]
-l2 = list(l1)
-l1.append(5)
-l2.append(5)
-print(l1, l2)
-print(l1 == l2)
-print(l1 is l2)
-print(type(l1))
-
-
