9c76f06fad8f9039e1660995279f51d9d1299c58
Cyril Vincent <contact@cyrilvincent.com>
2026-09-09 10:26:25 +0200

    Sparks

--- hello.py
+++ hello.py
@@ -2,3 +2,19 @@ import sys
 
 print("Hello")
 print(sys.version)
+
+a = 1
+b = a + 1
+print(b)
+print(type(b))
+c = 3.14
+print(type(c))
+
+if c < 4:
+    print("ok")
+else:
+    print("toto")
+
+for i in range(20, 10, -2):
+    print(i)
+
---
+++ tp_boucle.py
@@ -0,0 +1,8 @@
+n = 5
+# 1*2*3*4*5
+# n!
+
+n=7
+# n est il premier : 7 est divisible que par 1 et 7
+# Tout nombre > 1 est premier SAUF s'il possède un diviseur entre 2 et n-1
+print(9%3, 8%3)
