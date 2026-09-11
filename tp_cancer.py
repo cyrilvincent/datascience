e774d0b7fc8c0bdb0b463e8bdd6c2b4ef761ec80
Cyril Vincent <contact@cyrilvincent.com>
2026-09-11 15:52:51 +0200

    Sparks

--- tp_cancer.py
+++ tp_cancer.py
@@ -4,6 +4,7 @@ import sklearn.model_selection as ms
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.neighbors as n
+import sklearn.neural_network as nn
 import sklearn.ensemble as rf
 import sklearn.tree as tree
 import pickle
@@ -26,15 +27,16 @@ scaler.fit(xtrain)
 xtrain = scaler.transform(xtrain)
 xtest = scaler.transform(xtest)
 
-model = rf.RandomForestClassifier(n_estimators=100)
+# model = rf.RandomForestClassifier(n_estimators=100)
+model = nn.MLPClassifier(hidden_layer_sizes=(30,20,10))
 model.fit(xtrain, ytrain)
 
-print(model.feature_importances_)
-plt.bar(x.columns, model.feature_importances_)
-plt.xticks(rotation=45)
-plt.show()
-
-tree.export_graphviz(model.estimators_[0], out_file="data/breast-cancer/tree.dot", feature_names=x.columns, class_names=["0", "1"])
+# print(model.feature_importances_)
+# plt.bar(x.columns, model.feature_importances_)
+# plt.xticks(rotation=45)
+# plt.show()
+#
+# tree.export_graphviz(model.estimators_[0], out_file="data/breast-cancer/tree.dot", feature_names=x.columns, class_names=["0", "1"])
 
 print(f"Train score: {model.score(xtrain, ytrain)}")
 print(f"Test score: {model.score(xtest, ytest)}")
@@ -42,6 +44,6 @@ print(f"Test score: {model.score(xtest, ytest)}")
 ypred = model.predict(xtest)
 print(ypred)
 
-with open(f"data/breast-cancer/rf-{model.score(xtest, ytest):.2f}.pkl", "wb") as f:
+with open(f"data/breast-cancer/mlp-{model.score(xtest, ytest):.2f}.pkl", "wb") as f:
     pickle.dump([scaler, model], f)
 
